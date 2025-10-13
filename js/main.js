de6b26fb5d6383e0a43bbaebb695a4ba65225fcf
Godwin Kamau <contactgodwinkamau@gmail.com>
2025-10-13 17:11:11 -0400

    read directions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 document.querySelector('#wuMaker').addEventListener('click',generator)
+const radios = document.querySelectorAll('input[type="radio"]')
+
 
 function generator(){
     const first = document.querySelector('#firstname').value.toLowerCase()
     const last = document.querySelector('#lastname').value.toLowerCase()
+    radios.forEach(radio => {
+        if(radio.checked){
+            const radioValue = radio.value
+        }
+    })
+    console.log(document.querySelector('#color').value)
     fetch(`/namemaker?generator=${first}+${last}`)
         .then(res => res.text())
         .then(data => {
